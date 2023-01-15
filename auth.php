2e4860a1954f81f2d1b07f65393cc69db311c112
Varvara Sarkisova <varyasova03@gmail.com>
2023-01-15 18:28:14 +0300

    Login & authirization page

---
+++ auth.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Транспорт Москвы</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link rel="stylesheet" href="css/auth_style.css">
+</head>
+<body>
+    <form>
+            <h3 class="h3">Регистрация</h3>
+            <label for="username">Логин</label>
+            <input type="text" placeholder="Введите адрес электронной почты" id="username">
+            <label for="password">Пароль</label>
+            <input type="password" placeholder="Придумайте пароль" id="password">
+            <button>Зарегистрироваться</button>
+            <a href="auth.php" class="link" margin-left="35px">Уже есть аккаунт? Войти</a>
+        </form>    
+   
+</body>
+</html>
+
+
