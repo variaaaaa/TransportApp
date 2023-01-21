bc522bcb869305d8a47621eb6583b2a9e71b0bbb
Varvara Sarkisova <varyasova03@gmail.com>
2023-01-21 16:48:07 +0300

    Did the subway page with tables

--- subway.php
+++ subway.php
@@ -34,9 +34,10 @@ require("php\session.php");?>
 	<div class="navi">
 		<h3>Станции</h3>
 		<span>
-			<a href="#botoni1" class="botoniac">Пассажиропоток на каждой станции в 3 квартале 2022 года</a>
+        <a href="#botoni1" class="botoniac">Пассажиропоток на каждой станции в 3 квартале 2022 год</a>
 			<a href="#botoni2">Топ-5 самых загруженных станций. 3 квартал 2022 года</a>
-			<a href="diagrams.php">Диаграммы</a>
+			<a href="#botoni3">Топ-5 наименее загруженных станций. 3 квартал 2022 года
+			<a href="#botoni4">Диаграммы</a>
 		</span>
 		<h3>Линии</h3>
 		<span>
@@ -61,39 +62,36 @@ require("php\session.php");?>
                     </table>
                 </div>
             </div>           
-            <div id="botoni2">         
-                <h1>Диаграмма</h1>
-                <br> Топ-5 самых загруженных станций за 3 квартал 2022. </br>        
-                <div id="container">
-                    <script>
-                    anychart.onDocumentReady(function() {
-                    // set the data
-                    var data = [
-                        {x: "Комсомольская", value: 15244486},
-                        {x: "ВДНХ", value: 12608745},
-                        {x: "Щёлковская", value: 12412212},
-                        {x: "Новогиреево", value: 11261096},
-                        {x: "Выхино", value: 10990775}
-                    ];
-                    // create the chart
-                    var chart = anychart.pie();
-                    // set the chart title
-                    chart.title("Самые загруженные станции Московского метро за 3 квартал 2022 года");
-                    // add the data
-                    chart.data(data);
-                    // display the chart in the container
-                    chart.container('container');
-                    chart.draw();
-                    });
-                    </script>
-                    <div class = "topFive">
+            
+            <div id="botoni2" style="display: block;">      
+                <h1>Общая таблица</h1>
+                <br> В Москве 230+ станций. Какие-то из станций более популярны, чем другие. Но если посмотреть на пассажиропоток ВСЕХ станций в одном месте, ничего толком не понят (эта табличка и представлена после текста).</br>
+                <br margin-bottom="10px"> Поэтому в меню слева можно открыть интересующую информацию, представленную в виде таблицы и/или диаграммы.</br>
+                <br> А вот и табличка: </br>
+                <div class = "topFive">
+                    <table bordercolor = "black" bgcolor = "white" width = "500" align="center" text-color="black">
+                    <?php require "php\secondTable.php"; ?>
+                    </table>
                 </div>
-            </div>
-        </div>
-        <div id="botoni3">
+            </div> 
+            <div id="botoni3" style="display: block;">      
+                <h1>Общая таблица</h1>
+                <br> В Москве 230+ станций. Какие-то из станций более популярны, чем другие. Но если посмотреть на пассажиропоток ВСЕХ станций в одном месте, ничего толком не понят (эта табличка и представлена после текста).</br>
+                <br margin-bottom="10px"> Поэтому в меню слева можно открыть интересующую информацию, представленную в виде таблицы и/или диаграммы.</br>
+                <br> А вот и табличка: </br>
+                <div class = "topFive">
+                    <table bordercolor = "black" bgcolor = "white" width = "500" align="center" text-color="black">
+                    <?php require "php\anotherTable.php"; ?>
+                    </table>
                 </div>
-		</div>																									
-		
+            </div> 
+            <div id="botoni4" style="display: block;">      
+                <h1>Общая таблица</h1>
+                <br> В Москве 230+ станций. Какие-то из станций более популярны, чем другие. Но если посмотреть на пассажиропоток ВСЕХ станций в одном месте, ничего толком не понят (эта табличка и представлена после текста).</br>
+                
+            </div> 
+
+ 																						
 		
     </div>
     
